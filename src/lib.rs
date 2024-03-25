294e736aadd16e2b9ffc1feccbb0c27000b282fe
Jin Dong <jin.dong@databricks.com>
2024-03-24 22:03:49 -0700

    ch3: health check endpoint (#1)
    * ch3: health check endpoint
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    * ch3: db creation and migration (for UT/CI)
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    * ch3: add sqlx dep
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    * refactor to sub modules
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    * ch3: add config file
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    * ch3: add db connection and related tests
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    * ch3: update CI to enable db setup
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    * ch3: enable DB setup in test/CI
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    * ch3: update actions
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>
    
    ---------
    
    Signed-off-by: Jin Dong <jin.dong@databricks.com>

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
+// async fn _greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", &name)
+// }
