b8b37219dd2937ebfc5f8e035f815600d91f1152
Jin Dong <jin.dong@databricks.com>
2024-03-26 20:52:41 -0700

    ch4: change env logger to env filter from tracing
    Signed-off-by: Jin Dong <jin.dong@databricks.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use env_logger::Env;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // `init` does call `set_logger`.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+    // output formatted spans to stdout.
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(), std::io::stdout);
+    // The `with` method is provided by `SubscriberExt`, an extension
+    // trait for `Subscriber` exposed by `tracing_subscriber`.
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    // `set_global_default` can be used by applications to specify
+    // what subscriber should be used to process spans.
+    set_global_default(subscriber).expect("Failed to set subscriber");
     
     let configuration = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
