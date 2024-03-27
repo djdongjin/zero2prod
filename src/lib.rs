01fb8f66d9c4427c7bb5e5597eafb0ea9a53fcfb
Jin Dong <jin.dong@databricks.com>
2024-03-26 21:30:41 -0700

    ch4: move tracing to telemetry mod
    Signed-off-by: Jin Dong <jin.dong@databricks.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod configuration;
 pub mod routes;
 pub mod startup;
+pub mod telemetry;
 
 // async fn _greet(req: HttpRequest) -> impl Responder {
 //     let name = req.match_info().get("name").unwrap_or("World");
