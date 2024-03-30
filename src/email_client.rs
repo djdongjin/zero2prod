39df84e75929f89762247f235f8ea08acb1fda39
Jin Dong <djdongjin95@gmail.com>
2024-03-30 02:28:54 -0700

    ch7: tighten send_email test
    Signed-off-by: Jin Dong <djdongjin95@gmail.com>

--- src/email_client.rs
+++ src/email_client.rs
@@ -71,7 +71,7 @@ mod tests {
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
     use secrecy::Secret;
-    use wiremock::matchers::any;
+    use wiremock::matchers::{header, header_exists, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
 
     #[tokio::test]
@@ -83,7 +83,10 @@ mod tests {
 
         // mounting a Mock to the mock server.
         // Given we send one request, so we `expect(1)` here.
-        Mock::given(any())
+        Mock::given(header_exists("X-Postmark-Server-Token"))
+            .and(header("Content-Type", "application/json"))
+            .and(path("/email"))
+            .and(method("POST"))
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
