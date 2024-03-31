d7352dbcde67a51c1e0872cc83a3867594e52ece
Jin Dong <djdongjin95@gmail.com>
2024-03-31 11:54:34 -0700

    ch7: [test] token validation in confirm
    Signed-off-by: Jin Dong <djdongjin95@gmail.com>

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -39,3 +39,34 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
     // Assert
     assert_eq!(resp.status().as_u16(), 200);
 }
+
+#[tokio::test]
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Mock: if email server receives POST, return 200.
+    // This makes sure our confirm indeed sends an email to the email server (here, mocked).
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act 1: send a POST request to /subscriptions
+    // The response body should contain a link to confirm the subscription.
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let link = app.get_configuration_links(email_request);
+    let _ = reqwest::get(link.html).await.unwrap().error_for_status().unwrap();
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "confirmed");
+}
