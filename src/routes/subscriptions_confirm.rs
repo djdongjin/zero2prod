2b41650e31e515b11e092dbe2e345d28ec073c6d
Jin Dong <djdongjin95@gmail.com>
2024-03-30 22:22:07 -0700

    ch7: [test] subscribe + confirm
    Signed-off-by: Jin Dong <djdongjin95@gmail.com>

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,6 +1,11 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder};
 
-#[tracing::instrument(name = "Confirm a pending subscriber")]
-pub async fn confirm() -> impl Responder {
+#[derive(serde::Deserialize)]
+pub struct Parameters {
+    pub subscription_token: String,
+}
+
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(_params))]
+pub async fn confirm(_params: web::Query<Parameters>) -> impl Responder {
     HttpResponse::Ok().finish()
 }
--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,3 +1,9 @@
+use reqwest::Url;
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
@@ -9,4 +15,44 @@ async fn confirmation_without_token_are_rejected_with_a_400() {
         .expect("Failed to execute request.");
 
     assert_eq!(resp.status().as_u16(), 400);
+}
+
+#[tokio::test]
+async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Mock: if email server receives POST, return 200.
+    // This makes sure our confirm indeed sends an email to the email server (here, mocked).
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act 1: send a POST request to /subscriptions
+    // The response body should contain a link to confirm the subscription.
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+
+    // Extract the confirmation link
+    let raw_confirmation_link = &get_link(&body["HtmlBody"].as_str().unwrap());
+    let confirmation_link = Url::parse(raw_confirmation_link).unwrap();
+    // Let's make sure we don't call random APIs on the web
+    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+
+    // Act 2: call the returned link should send a GET to /subscriptions/confirm
+    let resp = reqwest::get(confirmation_link).await.unwrap();
+
+    // Assert
+    assert_eq!(resp.status().as_u16(), 200);
 }
