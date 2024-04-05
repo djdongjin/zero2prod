61a0bab31f91eeb639671d0f851263c437deb2a5
Jin Dong <djdongjin95@gmail.com>
2024-04-04 20:55:05 -0700

    ch9: add get_confirmed_subscribers
    Signed-off-by: Jin Dong <djdongjin95@gmail.com>

--- src/domain/newsletter.rs
+++ src/domain/newsletter.rs
@@ -1,5 +1,7 @@
-use actix_web::web;
-use actix_web::HttpResponse;
+use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
+use sqlx::PgPool;
+
+use crate::routes::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -13,6 +15,51 @@ pub struct Content {
     text: String,
 }
 
-pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+// Same logic to get the full error chain on `Debug`
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+pub async fn publish_newsletter(
+    _body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
+
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    // `query_as` maps results to the type specified as its first argument.
+    let confirmed_subscribers = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"SELECT email FROM subscriptions WHERE status = 'confirmed'"#
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(confirmed_subscribers)
 }
